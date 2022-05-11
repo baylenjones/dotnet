533af137cff5f7f1f95d65ef37720caba85b5a47
baylenjones <baylen.jones0224@gmail.com>
2022-05-11 09:40:56 -0500

    first commit

---
+++ csharp-basics/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var name = "testing";
+int age = 26;
+
+Console.WriteLine(name);
+
+if (age != null)
+{
+    Console.WriteLine(age);
+}
